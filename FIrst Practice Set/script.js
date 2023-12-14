9ddba81a0809291861cf4c532b7c69853157ea4a
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-14 11:56:53 +0530

    Added gsap to map 0 location to 255 intensity of the color in left side and same for right side . Changed the color using gsap.to

--- FIrst Practice Set/script.js
+++ FIrst Practice Set/script.js
@@ -5,13 +5,21 @@ rect.addEventListener("mousemove",function(details){
     // console.log(rect_location);
     // console.log(details.clientX);
     // this will give us x value only inside container
-    var inside_rect = details.clientX
+    var inside_rect = details.clientX-rect_location.left
     
     // check if mouse is inside container and behind center
-    if((inside_rect-rect_location.left)<rect_location.width/2){
-        console.log("Left");
+    if(inside_rect<rect_location.width/2){
+        var redColor = gsap.utils.mapRange(0,rect_location.width/2,255,0,inside_rect)
+        gsap.to(rect,{
+            backgroundColor: `rgb(${redColor},0,0)`,
+            ease: Power4
+        })
     }
     else{
-        console.log("Right");
+        var blueColor = gsap.utils.mapRange(rect_location.width/2,rect_location.width,0,255,inside_rect)
+        gsap.to(rect,{
+            backgroundColor: `rgb(0,0,${blueColor})`,
+            ease: Power4
+        })
     }
 })

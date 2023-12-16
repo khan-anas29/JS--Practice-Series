d9de279207bfd55cea30da89aff1315d2f7f79f4
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-16 17:09:54 +0530

    Added the image card

--- Third practice Set/script.js
+++ Third practice Set/script.js
@@ -26,6 +26,14 @@ container.addEventListener("mousemove",
         // add it to body
         document.body.appendChild(div)
 
+        // create img
+        var img=document.createElement("img")
+        // adding source
+        img.setAttribute("src","https://images.unsplash.com/photo-1702223175052-b577143bf2c4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+        // Adding image to div
+        div.appendChild(img)
+
         // clear it after some time
         setTimeout(function(){
             div.remove()

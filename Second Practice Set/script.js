39a58d9710e5f0c3be914ea4eb93cef254de180b
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-16 16:05:36 +0530

    Made the circle half to make it more inside thus successfully make the animation work

--- Second Practice Set/script.js
+++ Second Practice Set/script.js
@@ -1,9 +1,10 @@
 // We need the mouse on complete screen not only inside the container part thus selecting window
 
 window.addEventListener("mousemove",function(details){
+    var circle= document.querySelector(".container")
 
-    // shortening range of circle to be inside the mouse when mouse it at the extreme points (details.clientX is the mouse location)
-    var xval=gsap.utils.mapRange(0,window.innerWidth,100,window.innerWidth-100,details.clientX)
+    // half circle inside plus 100 points more inside thus to make it behind the mouse
+    var xval=gsap.utils.mapRange(0,window.innerWidth,100 + circle.getBoundingClientRect().width/2, window.innerWidth-(100 +circle.getBoundingClientRect().width/2),details.clientX)
 
     gsap.to(".container",{
         left: xval+"px",
